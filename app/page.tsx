c415aea965771407f1ddc6a1959eca22fef6a972
slawless423 <slawless@fordham.edu>
2026-02-13 12:03:18 -0500

    add home page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1 style={{ fontSize: 28, fontWeight: 700 }}>Women’s KenPom (Beta)</h1>
+      <p>If you can see this page, Next.js is working.</p>
+      <p>Next step: we’ll add the ratings table.</p>
+    </main>
+  );
+}
