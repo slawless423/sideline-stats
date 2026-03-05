6d9698b602c33dccc3e7f34f019ace13c27cad7b
slawless423 <slawless@fordham.edu>
2026-03-05 11:09:35 -0500

    Update page.tsx

--- app/mens-d1/players/page.tsx
+++ app/mens-d1/players/page.tsx
@@ -431,7 +431,7 @@ export default function MensD1PlayersPage() {
                     })}
                   </tr>
                 );
-              }))}
+              })}
             </tbody>
           </table>
         </div>
