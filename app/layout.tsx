ec9820e04efeb7c62662e2301e4a6b5671337473
slawless423 <slawless@fordham.edu>
2026-03-26 14:37:25 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 export const metadata = {
   title: "Sideline Stats",
@@ -15,6 +16,18 @@ export default function RootLayout({
       <body style={{ margin: 0, fontFamily: "system-ui" }}>
         {children}
         <Analytics />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-8E9VLX2LRX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8E9VLX2LRX');
+          `}
+        </Script>
       </body>
     </html>
   );
