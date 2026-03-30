767eef8f5fd2832251b21a66529b3d48c09d9917
slawless423 <slawless@fordham.edu>
2026-03-30 11:08:57 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['pdfkit'],
+  async headers() {
+    return [
+      {
+        source: '/api/recruiting/njcaa-import',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, x-import-secret' },
+        ],
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
